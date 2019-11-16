e9ab7e690d48ccaab7a600b93cc422cf0744a2ad
Fran Tamarit <fcodpaula.tamarit@gmail.com>
2019-11-16 18:43:24 +0100

    challenge 1 solved

---
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -0,0 +1,6 @@
+-- Challenge 1
+select authors.au_id AS AUTHOR_ID, au_lname AS LAST_NAME, au_fname AS FIRST_NAME, titles.title AS TITLE, publishers.pub_name AS PUBLISHER
+from titles
+join titleauthor on titleauthor.title_id = titles.title_id
+join authors on authors.au_id = titleauthor.au_id
+join publishers on titles.pub_id = publishers.pub_id
